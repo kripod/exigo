f1ccfad7759ec637ac249b871548d001c0e9ab57
Kristóf Poduszló <kripod@protonmail.com>
2019-09-07 17:17:59 +0200

    Add Chakra Design System

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,7 +1,6 @@
+import { theme as defaultTheme } from '@chakra-ui/core';
 import styled, { CreateStyled } from '../../node_modules/@emotion/styled';
 
-export const theme = {
-  /* TODO */
-};
+export const theme = defaultTheme;
 
 export default styled as CreateStyled<typeof theme>;
