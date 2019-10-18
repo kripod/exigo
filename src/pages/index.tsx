9cbf2061494d0f1a18b8e6e11672f8875720e24e
Kristóf Poduszló <kripod@protonmail.com>
2019-10-18 21:28:33 +0200

    Add sample question bank

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,23 +4,23 @@ import Layout from '../components/Layout';
 import MultipleChoiceForm from '../components/MultipleChoiceForm';
 import Option from '../models/Option';
 
+import multipleChoiceQuizExample from '../data/examples/multipleChoiceQuiz.json';
+
 export default function IndexPage() {
   const [solutionID, setSolutionID] = useState<Option['id']>();
+  const item = multipleChoiceQuizExample.items[0];
 
   return (
     <Layout>
-      <QuizItemCard stem="If a=1 and b=2, what is a+b?" mx="auto">
+      <QuizItemCard stem={item.stem} mx="auto">
         <MultipleChoiceForm
-          choices={[
-            { id: 1, text: '1' },
-            { id: 2, text: '3' },
-            { id: 3, text: '8' },
-            { id: 4, text: '12' },
-          ]}
+          choices={item.choices}
           solutionID={solutionID}
           onChange={() => {
             /* TODO: Get solutionID from server instead of simulated delay */
-            if (!solutionID) setTimeout(() => setSolutionID(2), 800);
+            if (!solutionID) {
+              setTimeout(() => setSolutionID(item.solutionID), 800);
+            }
           }}
         />
       </QuizItemCard>
