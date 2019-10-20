2ce93382895bcec51c00c6c84b80f1efa540b2e4
Kristóf Poduszló <kripod@protonmail.com>
2019-10-20 23:45:26 +0200

    Make inactive carousel slide inert

--- src/components/QuizItemCardSet.tsx
+++ src/components/QuizItemCardSet.tsx
@@ -7,6 +7,9 @@ import Measure from './Measure';
 import MultipleOptionsEvaluator from './MultipleOptionsEvaluator';
 import QuizItemCard from './QuizItemCard';
 
+// TODO: Follow the status of https://github.com/WICG/inert and remove polyfill
+import 'wicg-inert';
+
 const evaluatorComponents = new Map([
   [QuizItemType.MULTIPLE_OPTIONS, MultipleOptionsEvaluator],
 ]);
@@ -24,23 +27,27 @@ export default function QuizItemCardSet({
 
   return (
     <Carousel as={Measure} spacingX={4} spacingY={6} {...restProps}>
-      {items.map(item => {
+      {items.map((item, i) => {
         // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
         const Evaluator = evaluatorComponents.get(item.type)!;
 
         return (
+          // TODO: Auto-manage `inert` prop through `Carousel`
+          // TODO: Remove extra `div` once `shouldForwardProp` of `Box` supports `inert`
           <CarouselSlide>
-            <QuizItemCard stem={item.stem}>
-              <Evaluator
-                {...item}
-                onChange={response => {
-                  setResponses(prevResponses => ({
-                    ...prevResponses,
-                    [index]: response,
-                  }));
-                }}
-              />
-            </QuizItemCard>
+            <div inert={i !== index ? '' : undefined}>
+              <QuizItemCard stem={item.stem}>
+                <Evaluator
+                  {...item}
+                  onChange={response => {
+                    setResponses(prevResponses => ({
+                      ...prevResponses,
+                      [index]: response,
+                    }));
+                  }}
+                />
+              </QuizItemCard>
+            </div>
           </CarouselSlide>
         );
       })}
