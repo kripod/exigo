5da1093bbc8e20e90b3085769a203d6024ac6506
Kristóf Poduszló <kripod@protonmail.com>
2019-10-21 20:03:07 +0200

    Make Carousel work as an uncontrolled component

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CarouselContainer from './CarouselContainer';
 import CarouselControls from './CarouselControls';
 import CarouselRotator, { CarouselRotatorProps } from './CarouselRotator';
 
-export default function Carousel(props: CarouselRotatorProps) {
+interface CarouselProps extends CarouselRotatorProps {
+  initialIndex?: number;
+}
+
+export default function Carousel({
+  initialIndex = 0,
+  ...props
+}: CarouselProps) {
+  const [activeIndex, setActiveIndex] = useState(initialIndex);
+
   return (
     <CarouselContainer>
       <CarouselControls />
-      <CarouselRotator {...props} />
+      <CarouselRotator
+        activeIndex={activeIndex}
+        {...props} // `activeIndex` is overridable from here
+      />
     </CarouselContainer>
   );
 }
