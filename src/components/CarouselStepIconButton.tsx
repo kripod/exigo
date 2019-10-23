b5b71ab3eb6c8e2a23c7661bff03a7728893f18a
Kristóf Poduszló <kripod@protonmail.com>
2019-10-23 11:14:57 +0200

    Implement CarouselStepIconButton functionality

--- src/components/CarouselStepIconButton.tsx
+++ src/components/CarouselStepIconButton.tsx
@@ -1,6 +1,28 @@
 import { IconButton, IconButtonProps } from '@chakra-ui/core';
-import React from 'react';
+import React, { useCallback } from 'react';
+import useCarouselControls from '../hooks/useCarouselControls';
 
-export default function CarouselStepIconButton(props: IconButtonProps) {
-  return <IconButton variant="ghost" size="lg" isRound {...props} />;
+export interface CarouselStepIconButtonProps extends IconButtonProps {
+  delta: number;
+}
+
+export default function CarouselStepIconButton({
+  delta,
+  ...props
+}: CarouselStepIconButtonProps) {
+  const { jump } = useCarouselControls();
+
+  // TODO: Disable button if necessary when the carousel is not infinite
+  return (
+    <IconButton
+      variant="ghost"
+      size="lg"
+      fontSize="3xl"
+      isRound
+      onClick={useCallback(() => {
+        jump(delta);
+      }, [delta, jump])}
+      {...props}
+    />
+  );
 }
