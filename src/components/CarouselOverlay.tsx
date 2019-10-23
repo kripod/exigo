f651b301ac3f2beedcb0ffc6f7b34a38e8594855
Kristóf Poduszló <kripod@protonmail.com>
2019-10-23 11:05:34 +0200

    Improve composability of CarouselOverlay

--- src/components/CarouselOverlay.tsx
+++ src/components/CarouselOverlay.tsx
@@ -2,24 +2,42 @@ import { Box, BoxProps } from '@chakra-ui/core';
 import React from 'react';
 import CarouselStepIconButton from './CarouselStepIconButton';
 
-export default function CarouselOverlay(props: BoxProps) {
+export interface CarouselOverlayProps extends BoxProps {
+  children?: React.ReactElement[];
+}
+
+export default function CarouselOverlay({
+  children = [
+    // TODO: Add play/pause button
+    <CarouselStepIconButton
+      delta={+1}
+      aria-label="Next slide"
+      icon="chevron-right"
+      top="50%"
+      right={4}
+      transform="translateY(-50%)"
+    />,
+    <CarouselStepIconButton
+      delta={-1}
+      aria-label="Previous slide"
+      icon="chevron-left"
+      top="50%"
+      left={4}
+      transform="translateY(-50%)"
+    />,
+  ],
+  ...props
+}: CarouselOverlayProps) {
   return (
-    <Box css={{ '& > *': { position: 'absolute', zIndex: 1 } }} {...props}>
-      {/* TODO: Add play/pause button */}
-      <CarouselStepIconButton
-        aria-label="Next slide"
-        icon="chevron-right"
-        top="50%"
-        right={4}
-        transform="translateY(-50%)"
-      />
-      <CarouselStepIconButton
-        aria-label="Previous slide"
-        icon="chevron-left"
-        top="50%"
-        left={4}
-        transform="translateY(-50%)"
-      />
+    <Box {...props}>
+      {React.Children.map(children, child =>
+        React.cloneElement(child, {
+          css: {
+            position: 'absolute',
+            zIndex: 1,
+          },
+        }),
+      )}
     </Box>
   );
 }
