9deec7f95d9ccdaf3e87baf113361a8a94f959da
Kristóf Poduszló <kripod@protonmail.com>
2019-10-24 18:54:29 +0200

    Improve CarouselIconButton color scheme

--- src/components/CarouselIconButton.tsx
+++ src/components/CarouselIconButton.tsx
@@ -1,16 +1,13 @@
-import { IconButton, IconButtonProps, useColorMode } from '@chakra-ui/core';
+import { IconButton, IconButtonProps } from '@chakra-ui/core';
 import React from 'react';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface CarouselIconButtonProps extends IconButtonProps {}
 
 export default function CarouselIconButton(props: CarouselIconButtonProps) {
-  const { colorMode } = useColorMode();
-  const isDarkMode = colorMode === 'dark';
-
   return (
     <IconButton
-      variantColor={isDarkMode ? 'whiteAlpha' : 'blackAlpha'}
+      variantColor="blackAlpha"
       color="white"
       size="lg"
       fontSize="3xl"
