653dd5d0552ce1fe98f259ab67d4f70e1f17812b
Kristóf Poduszló <kripod@protonmail.com>
2019-10-26 20:25:47 +0200

    Revert "Temporarily disable relative positioning"
    This reverts commit b7c0b10e18c9146ccc2beb2a445a71c291e3fd63.

--- src/components/CarouselContainer.tsx
+++ src/components/CarouselContainer.tsx
@@ -34,6 +34,7 @@ export default function CarouselContainer({
       <Box
         as="section"
         aria-roledescription="carousel"
+        position="relative"
         {...bindHover}
         {...bindFocus}
         {...restProps}
--- src/components/ScrollSnapContainer.tsx
+++ src/components/ScrollSnapContainer.tsx
@@ -87,7 +87,9 @@ export default function ScrollSnapContainer({
         scroll-snap-type: x mandatory;
         -ms-scroll-snap-points-x: snapInterval(0, 100%);
         scroll-snap-points-x: repeat(100%);
-        -webkit-overflow-scrolling: touch;
+
+        /* Disable iOS Safari momentum scrolling to avoid certain issues */
+        -webkit-overflow-scrolling: auto;
 
         /* TODO: Leave vendor prefixing to the underlying library */
         ::-webkit-scrollbar {
