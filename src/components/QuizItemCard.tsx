dbbb18d93bc363c300d6b8a2773dfe0092dc83c3
Kristóf Poduszló <kripod@protonmail.com>
2019-10-30 01:30:01 +0100

    Unify font weights

--- src/components/QuizItemCard.tsx
+++ src/components/QuizItemCard.tsx
@@ -19,7 +19,7 @@ export default function QuizItemCard({
         size="md"
         mx={QUIZ_ITEM_CARD_PADDING}
         py={QUIZ_ITEM_CARD_PADDING + 2}
-        fontWeight={500}
+        fontWeight={600}
         lineHeight={1.5}
         whiteSpace="pre-line"
         borderBottomWidth={1}
