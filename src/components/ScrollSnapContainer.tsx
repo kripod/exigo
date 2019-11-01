e4d48c799b3c38156a8fa4af7dbc3d5bc222fb11
Kristóf Poduszló <kripod@protonmail.com>
2019-11-01 18:01:30 +0100

    Temporarily disable iOS-specific behavior

--- src/components/ScrollSnapContainer.tsx
+++ src/components/ScrollSnapContainer.tsx
@@ -42,7 +42,7 @@ export default function ScrollSnapContainer({
   );
   // Handle occasional reflow prior to layout
   // See: https://openradar.appspot.com/radar?id=5040881597939712
-  const isWidthChanging = useChanging(width);
+  // const isWidthChanging = useChanging(width);
   const disableScrollPositionTracking = useRef(false);
   useLayoutEffect(() => {
     if (targetIndex != null) {
@@ -54,7 +54,7 @@ export default function ScrollSnapContainer({
 
     // Changing indexes shall not have an effect on scroll restoration
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [width, isWidthChanging]);
+  }, [width /* isWidthChanging */]);
 
   // TODO: Replace this check with CSS when no polyfill is required
   const preferReducedMotion = usePreferredMotionIntensity() === 'reduce';
