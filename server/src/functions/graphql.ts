4c842a167e16663989d974f223f631e07b778188
Kristóf Poduszló <kripod@protonmail.com>
2019-11-19 00:31:08 +0100

    Try fixing server deployment

--- server/src/functions/graphql.ts
+++ server/src/functions/graphql.ts
@@ -1,3 +1,8 @@
+// TODO: Remove when zip-it-and-ship-it gets fixed
+// See: https://github.com/netlify/zip-it-and-ship-it/issues/67#issuecomment-549837499
+// eslint-disable-next-line import/no-extraneous-dependencies
+import '@generated/photon';
+
 import { ApolloServer } from 'apollo-server-lambda';
 
 import context from '../graphql/context';
