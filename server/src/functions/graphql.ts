b9f1579b5ef2ae6511ced75765d860aec9b17625
Kristóf Poduszló <kripod@protonmail.com>
2019-11-18 17:22:17 +0100

    Fix serving dev server statically

--- server/src/functions/graphql.ts
+++ server/src/functions/graphql.ts
@@ -6,4 +6,4 @@ import schema from '../graphql/schema';
 const server = new ApolloServer({ schema, context });
 
 // eslint-disable-next-line import/prefer-default-export
-export const graphqlHandler = server.createHandler();
+export const handler = server.createHandler();
