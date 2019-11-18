c5adc24af59840e38f7b40797e0f8ebaaff5564d
Kristóf Poduszló <kripod@protonmail.com>
2019-11-19 00:20:05 +0100

    Prioritize per-project ESLint configs over root

---
+++ client/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: 'airbnb-typescript',
+  env: {
+    browser: true,
+  },
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'react/jsx-props-no-spreading': 'off',
+  },
+};
