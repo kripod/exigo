edf230ba82353061dbbd121b43f906acaa16f7aa
Kristóf Poduszló <kripod@protonmail.com>
2019-11-19 15:11:29 +0100

    Temporarily remove schema from GraphQL server

--- server/src/functions/graphql/graphql.ts
+++ server/src/functions/graphql/graphql.ts
@@ -1,9 +1,9 @@
 import { ApolloServer } from 'apollo-server-lambda';
 
 import context from './context';
-import schema from './schema';
+// import schema from './schema';
 
-const server = new ApolloServer({ schema, context });
+const server = new ApolloServer({ /* schema, */ context });
 
 // eslint-disable-next-line import/prefer-default-export
 export const handler = server.createHandler();
