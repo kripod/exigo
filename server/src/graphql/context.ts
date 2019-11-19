9d0cb242a0650d040800aa53cfd1fb9255be31cb
Kristóf Poduszló <kripod@protonmail.com>
2019-11-19 02:09:46 +0100

    Fix Netlify function deployment

--- server/src/graphql/context.ts
+++ server/src/graphql/context.ts
@@ -1,5 +1,8 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { Photon } from '@generated/photon';
+// TODO: Replace with the @prisma/photon facade package
+// See: https://github.com/prisma/photonjs/issues/261
+// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+// @ts-ignore
+import { Photon } from './.generated/photon'; // eslint-disable-line import/no-unresolved
 
 export type Context = {
   photon: Photon;
