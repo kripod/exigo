b7f50e8d257e23a19c1e9f96cb76223ab3afcb07
Kristóf Poduszló <kripod@protonmail.com>
2019-11-19 15:10:15 +0100

    Revert "Try fixing test function"
    This reverts commit 449c229734a0c6437bce4906e259d54e01f3a645.

--- server/src/functions/graphql/graphql.ts
+++ server/src/functions/graphql/graphql.ts
@@ -1,23 +1,9 @@
-import { ApolloServer, gql } from 'apollo-server-lambda';
+import { ApolloServer } from 'apollo-server-lambda';
 
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
+import context from './context';
+import schema from './schema';
 
-const resolvers = {
-  Query: {
-    hello: (parent: any, args: any, context: any): string => {
-      return 'Hello, world!';
-    },
-  },
-};
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
+const server = new ApolloServer({ schema, context });
 
 // eslint-disable-next-line import/prefer-default-export
 export const handler = server.createHandler();
